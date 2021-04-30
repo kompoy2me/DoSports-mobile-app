29703e136ce3a805bee543a44009f68ce722be45
kompoy2me <boshmachkin@gmail.com>
2021-04-30 11:41:32 +1000

    creating prototype of diary

--- www/js/add_prod_to_diary.js
+++ www/js/add_prod_to_diary.js
@@ -3,21 +3,59 @@
 });*/
 
 function add_to(name, prot, fats, harb, cell, cal){
-	
+
 	main = document.getElementById('display');
 
 	let table = document.createElement('div');
 	table.id = "table_prod";
 	table.addEventListener("click", function(){
+		
 		let modal = document.getElementById('modal');
 		modal.style.display = 'block';
+		modal.style.opacity = 1;
+		modal.style.zIndex = 100;
 		document.getElementById('announ_m').innerHTML = "Укажите количество продукта <b>" + name + "</b> в граммах";
+
 		document.getElementById('prot_count').innerHTML = "Б " + prot;
 		document.getElementById('fats_count').innerHTML = "Ж " + fats;
 		document.getElementById('harb_count').innerHTML = "У " + harb;
 		document.getElementById('cell_count').innerHTML = "Кл " + cell;
 		document.getElementById('cal_count').innerHTML = "Ка " + cal;
+
+		let quant = document.getElementById('quant');
+		quant.value = 100;
+		quant.onkeyup = function(){
+		if ((quant.value < 10000) & (quant.value > 0)){
+			document.getElementById('quant_prod').style.borderBottom = '2px solid #CCCCCC';
+			document.getElementById('prot_count').innerHTML = "Б " + ((prot/100)*quant.value).toFixed(2);
+			document.getElementById('fats_count').innerHTML = "Ж " + ((fats/100)*quant.value).toFixed(2);
+			document.getElementById('harb_count').innerHTML = "У " + ((harb/100)*quant.value).toFixed(2);
+			document.getElementById('cell_count').innerHTML = "Кл " + ((cell/100)*quant.value).toFixed(2);
+			document.getElementById('cal_count').innerHTML = "Ка " + ((cal/100)*quant.value).toFixed(2);
+		}
+		else{
+			document.getElementById('quant_prod').style.borderBottom = '2px solid #C20DB7';
+		}
+	};
+		sub =  document.getElementById('sub_m');
+		sub.onclick = function(){
+			if ((quant.value < 10000) & (quant.value > 0)){
+				var arr = JSON.parse(localStorage.getItem('array_prods'));
+				arr.push(new Array([name,
+				 ((prot/100)*quant.value).toFixed(2),
+				 ((fats/100)*quant.value).toFixed(2),
+				 ((harb/100)*quant.value).toFixed(2),
+				 ((cell/100)*quant.value).toFixed(2),
+				 ((cal/100)*quant.value).toFixed(2),
+				 quant.value]));
+				localStorage.setItem('array_prods',JSON.stringify(arr));
+				load_p("create_meal.html");
+			}
+  			
+		};
+
 	});
+	
 	main.append(table);
 	table.insertAdjacentHTML('beforeend', '<div id="table_prod" ><div id="head_table_prod"><div id="name_prod">'+
 							    	name+'</div></div><div id="stat_prod"><div id="prot_prod" class="elem_prod">Б ' +
@@ -27,6 +65,35 @@ function add_to(name, prot, fats, harb, cell, cal){
 							    	cell+'</div><div id="cal_prod">Ка '+
 							    	cal+'</div></div></div>');
 
+	
+	/*let sub = document.createElement('button');
+
+	sub.id = "sub_m";
+	sub.className = "sub_butt";
+	sub.innerHTML = "Принять";
+	sub.addEventListener("click", function(){
+		alert(name);
+	});
+	buttons = document.getElementById('buttons');
+	buttons.append(sub);
+	*/
+
+
+	/*
+
+		sub =  document.getElementById('sub_m');
+		sub.addEventListener("click", function(){
+			alert(name);
+  			var arr = [];
+			arr.push(new Array([name, prot, fats, harb, cell, cal]));
+
+			console.log(arr[0]);
+			localStorage.setItem('array',JSON.stringify(arr));
+			arr = JSON.parse(localStorage.getItem('array'));
+			console.log(arr[0]);
+			window.localStorage.removeItem('array');
+  		});
+	*/
 
 	/*let table_head = document.createElement('div');
 	table_head.id = "head_table_prod";
